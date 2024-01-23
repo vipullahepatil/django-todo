8c5f3669b3303f429f5fc5323fbd74533262f987
Vipul Lahe <vipul.lahe@indiabonds.com>
2024-01-24 01:19:22 +0530

    model setup done

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'todoApp.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todoapp',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- todos/models.py
+++ todos/models.py
@@ -1,10 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Todo(models.Model):
     title = models.CharField(max_length=100)
     created_at = models.DateTimeField('Created', auto_now_add=True)
     update_at = models.DateTimeField('Updated', auto_now=True)
     isCompleted = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    notification_time = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.title
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    phone = models.CharField(max_length=15, null=True, blank=True)
