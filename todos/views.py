3a87c09493dd117a8a504e6cde556596e3796d48
Vipul Lahe <vipul.lahe@indiabonds.com>
2024-01-24 17:02:38 +0530

    add page separated list/updated still in same

--- todos/views.py
+++ todos/views.py
@@ -1,24 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
-from .models import Todo,User
-from django.http import HttpResponseRedirect
+from .models import Todo
 from django.contrib.auth.decorators import login_required
 
-# @login_required
-class IndexView(generic.ListView):
-    template_name = 'todos/index.html'
-    context_object_name = 'todo_list'
- 
-    def get_queryset(self):
-        """Return all the latest todos."""
-        # return Todo.objects.order_by('-created_at')
-        return Todo.objects.filter(user=self.request.user).order_by('-created_at')
-
-def delete(request, todo_id):
-    todo = get_object_or_404(Todo, pk=todo_id)
-    todo.delete()
-
-    return redirect('todos:index')
 
 @login_required
 def add(request):
@@ -37,13 +21,23 @@ def add(request):
             user=user
         )
 
-    return redirect('todos:index')
+        return redirect('todos:index')
+
+    return render(request, 'todos/add.html')
+
+class IndexView(generic.ListView):
+    template_name = 'todos/index.html'
+    context_object_name = 'todo_list'
+    
+    def get_queryset(self):
+        return Todo.objects.filter(user=self.request.user).order_by('-created_at')
+
 
 @login_required
 def update(request, todo_id):
     todo = get_object_or_404(Todo, pk=todo_id)
 
-    if request.method == 'POST':
+    if request.method == 'POST': 
         title = request.POST['title']
         description = request.POST['description']
         deadline = request.POST.get('deadline', None)
@@ -62,5 +56,12 @@ def update(request, todo_id):
         todo.isCompleted = isCompleted
 
         todo.save()
+
+    return redirect('todos:index')
+
+
+def delete(request, todo_id):
+    todo = get_object_or_404(Todo, pk=todo_id)
+    todo.delete()
 
     return redirect('todos:index')
