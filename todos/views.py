ff9cf7abdaa89337837277c35445e2a7464df46a
Vipul Lahe <vipul.lahe@indiabonds.com>
2024-01-24 12:27:25 +0530

    till now user seperated todo show/user separation done

--- todos/views.py
+++ todos/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
-from .models import Todo
+from .models import Todo,User
 from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
+# @login_required
 class IndexView(generic.ListView):
     template_name = 'todos/index.html'
     context_object_name = 'todo_list'
-
+ 
     def get_queryset(self):
         """Return all the latest todos."""
-        return Todo.objects.order_by('-created_at')
+        # return Todo.objects.order_by('-created_at')
+        return Todo.objects.filter(user=self.request.user).order_by('-created_at')
 
 def add(request):
     title = request.POST['title']
-    Todo.objects.create(title=title)
+    user=request.user
+    # Todo.objects.create(title=title)
+    Todo.objects.create(title=title,user=user)
 
     return redirect('todos:index')
 
