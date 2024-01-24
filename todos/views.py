bcf299f0c90cb007987e430c3f69b452e33672bf
Vipul Lahe <vipul.lahe@indiabonds.com>
2024-01-24 13:22:43 +0530

    deleted comments of add updates users

--- todos/views.py
+++ todos/views.py
@@ -14,32 +14,11 @@ class IndexView(generic.ListView):
         # return Todo.objects.order_by('-created_at')
         return Todo.objects.filter(user=self.request.user).order_by('-created_at')
 
-# def add(request):
-#     title = request.POST['title']
-#     user=request.user
-#     # Todo.objects.create(title=title)
-#     Todo.objects.create(title=title,user=user)
-
-#     return redirect('todos:index')
-    
-
-
 def delete(request, todo_id):
     todo = get_object_or_404(Todo, pk=todo_id)
     todo.delete()
 
     return redirect('todos:index')
-
-# def update(request, todo_id):
-#     todo = get_object_or_404(Todo, pk=todo_id)
-#     isCompleted = request.POST.get('isCompleted', False)
-#     if isCompleted == 'on':
-#         isCompleted = True
-    
-#     todo.isCompleted = isCompleted
-
-#     todo.save()
-#     return redirect('todos:index')
 
 @login_required
 def add(request):
